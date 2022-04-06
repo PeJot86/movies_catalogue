0fef011fe4f2e61a27da2689c1a94eacadc739f8
Przemysław Jarocki <przemek.jarocki@gmail.com>
2022-04-06 14:00:53 +0200

    First version of my project

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    movies = []
+    for i in range (1,9):
+        movies.append(i)
+    return render_template("homepage.html", movies=movies)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
