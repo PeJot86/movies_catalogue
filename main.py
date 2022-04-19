f3dff9a344b22afa6375b23db360faad2247f542
Przemysław Jarocki <przemek.jarocki@gmail.com>
2022-04-19 11:09:27 +0200

    Version with lists

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ import tmdb_client
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
 def homepage():
-    movies = tmdb_client.get_movies(8)  
-    return render_template("homepage.html", movies=movies)
+    selected_list = request.args.get('list_type', "popular")
+    movies = tmdb_client.get_movies(how_many=8, type_list=selected_list)
+    return render_template("homepage.html", movies=movies, current_list=selected_list)
 
 
 @app.context_processor
@@ -23,6 +26,7 @@ def movie_details(movie_id):
    details = tmdb_client.get_single_movie(movie_id)
    cast = tmdb_client.get_single_movie_cast(movie_id)
    return render_template("movie_details.html", movie=details, cast=cast)
+
 
 
 if __name__ == '__main__':
--- tmdb_client.py
+++ tmdb_client.py
@@ -4,15 +4,14 @@ import random
 API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MWQyMWEyMTYxOGY4YTZjYzQ4YTg0OTc0OTBmMzM1MCIsInN1YiI6IjYyNGQ1NThmNGNiZTEyMDA2NWI5ZGQxOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gKlIXnw03n5gXXr_KkkVoKUCzgT-iR-tJsE0JQhoavw" 
 
 
-def get_popular_movies():
-    endpoint = "https://api.themoviedb.org/3/movie/popular"
+def get_movies_list(list_type):
+    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
     headers = {
         "Authorization": f"Bearer {API_TOKEN}"
     }
     response = requests.get(endpoint, headers=headers)
     response.raise_for_status()
-    return (response.json())
-
+    return response.json()
 
 
 def get_poster_url(poster_api_path, size="w342"):
@@ -20,8 +19,15 @@ def get_poster_url(poster_api_path, size="w342"):
     return f"{base_url}{size}/{poster_api_path}"
 
 
-def get_movies(how_many):
-    data = get_popular_movies()
+def get_movies(how_many, type_list):
+    if type_list == 'popular':
+        data = get_movies_list('popular')
+    elif type_list == 'top_rated':
+        data =  get_movies_list('top_rated')
+    elif type_list == 'upcoming':
+        data =  get_movies_list('upcoming')
+    elif type_list == 'now_playing':
+        data =  get_movies_list('now_playing')
     random.shuffle (data["results"])
     return data["results"][:how_many]
 
